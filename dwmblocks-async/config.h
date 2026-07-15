e2f663ca4c61b299402b3fd2bbe2c826aa043f71
Icaro Meira <icaro.meira.oi@gmail.com>
2026-07-15 06:21:37 +0000

    Adiciona uma dwm bar simples

---
+++ dwmblocks-async/config.h
@@ -0,0 +1,26 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// String used to delimit block outputs in the status.
+#define DELIMITER " | "
+
+// Maximum number of Unicode characters that a block can output.
+#define MAX_BLOCK_OUTPUT_LENGTH 45
+
+// Control whether blocks are clickable.
+#define CLICKABLE_BLOCKS 0
+
+// Control whether a leading delimiter should be prepended to the status.
+#define LEADING_DELIMITER 1
+
+// Control whether a trailing delimiter should be appended to the status.
+#define TRAILING_DELIMITER 1
+
+// Define blocks for the status feed as:
+//  X(icon, cmd,                 interval, signal).
+#define BLOCKS(X)             \
+    X("",   "dwmblocks-volume",  0,        5)  \
+    X("",   "dwmblocks-battery", 60,       1)  \
+    X("",   "dwmblocks-time",    60,       1)
+
+#endif  // CONFIG_H
